7a380ee909aeb417dde0afb3fe0fa07c84f39de3
Victor611 <sorokin.victor.v@gmail.com>
2021-10-04 08:52:27 +0300

    goauth

--- controller/authController.js
+++ controller/authController.js
@@ -2,15 +2,14 @@ const querystring = require('query-string');
 const axios = require('axios');
 const jwt = require('jsonwebtoken');
 var express = require('express');
-const {GOOGLE_CLIENT_ID, SERVER_ROOT_URI, GOOGLE_CLIENT_SECRET, JWT_SECRET, COOKIE_NAME, UI_ROOT_URI, REDIRECT_URI} = require ('../config');
-
-
-
+const {SERVER_ROOT_URI, JWT_SECRET, UI_ROOT_URI,} = require('../config');
+const {GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REDIRECT_URI} = require ('../config');
+const {FACEBOOK_CLIENT_ID, FACEBOOK_CLIENT_SECRET, FACEBOOK_REDIRECT_URI} = require ('../config');
 
 module.exports.getGoogleAuthURL = () => {
-  const rootUrl = "https://accounts.google.com/o/oauth2/v2/auth";
-  const options = {
-    redirect_uri: `${SERVER_ROOT_URI}/${REDIRECT_URI}`,
+  const rootGoogleOauthUrl = "https://accounts.google.com/o/oauth2/v2/auth";
+  const google_options = {
+    redirect_uri: `${SERVER_ROOT_URI}/${GOOGLE_REDIRECT_URI}`,
     client_id: GOOGLE_CLIENT_ID,
     access_type: "offline",
     response_type: "code",
@@ -20,7 +19,7 @@ module.exports.getGoogleAuthURL = () => {
       "https://www.googleapis.com/auth/userinfo.email",
     ].join(" "),
   };
-  return `${rootUrl}?${querystring.stringify(options)}`;
+  return `${rootGoogleOauthUrl}?${querystring.stringify(google_options)}`;
 }
 
 module.exports.getCurrentUser = (req, res) => {
@@ -37,7 +36,7 @@ module.exports.getCurrentUser = (req, res) => {
 
 module.exports.getGoogleUserFromCode = (req, res) => {
   const code = req.body.code.toString();
-  getTokens({
+  getGoogleTokens({
     code,
     clientId: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
@@ -45,13 +44,8 @@ module.exports.getGoogleUserFromCode = (req, res) => {
   })
   .then((google_token)=>{
     const {id_token, access_token} = google_token
-    axios
-    .get( 
-      `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${access_token}`
-      ,{ headers: { Authorization: `Bearer ${id_token}`, },}
-    )
+    getGoogleDataByUser({id_token, access_token})
     .then((google_data) => {
-      console.log(google_data.data.id)
       /*запись в коллекцию Google res.data
         { id:, //via google_id
           user_id: , // к какому user относится
@@ -76,8 +70,7 @@ module.exports.getGoogleUserFromCode = (req, res) => {
   });
 }
 
-function getTokens({code, clientId, clientSecret, redirectUri}) {
-  
+function getGoogleTokens({code, clientId, clientSecret, redirectUri}) {
   const url = 'https://oauth2.googleapis.com/token';
   const values = {
     code,
@@ -88,7 +81,8 @@ function getTokens({code, clientId, clientSecret, redirectUri}) {
   };
 
   return axios
-    .post(url
+    .post(
+      url
       , querystring.stringify(values)
       , { headers: {'Content-Type': 'application/x-www-form-urlencoded',}}
     )
@@ -96,4 +90,68 @@ function getTokens({code, clientId, clientSecret, redirectUri}) {
     .catch((error) => {
       throw new Error(error.message);
     });
+}
+
+function getGoogleDataByUser({id_token, access_token}){
+  const url = `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${access_token}`;
+  return axios
+    .get( 
+      url
+      ,{ headers: { Authorization: `Bearer ${id_token}`, },}
+    )
+    .then((res) => res.data)
+    .catch((error) => {
+      throw new Error(error.message);
+    });
+}
+
+module.exports.getFacebookAuthURL = ()=>{
+  const rootFacebookOauthUrl = "https://www.facebook.com/v12.0/dialog/oauth";
+  const facebook_options = {
+    redirect_uri: `${SERVER_ROOT_URI}/${FACEBOOK_REDIRECT_URI}`,
+    client_id: FACEBOOK_CLIENT_ID,
+    response_type: "code",
+    auth_type: 'rerequest',
+    scope: ['email', 'public_profile'].join(","),
+    display: "popup",
+   
+  };
+  return `${rootFacebookOauthUrl}?${querystring.stringify(facebook_options)}`
+}
+
+module.exports.getFacebookUserFromCode = (req, res) => {
+  const code = req.body.code.toString();
+ console.log('f_code', code)
+  // getGoogleTokens({
+  //   code,
+  //   clientId: GOOGLE_CLIENT_ID,
+  //   clientSecret: GOOGLE_CLIENT_SECRET,
+  //   redirectUri: `${SERVER_ROOT_URI}/${REDIRECT_URI}`,
+  // })
+  // .then((google_token)=>{
+  //   const {id_token, access_token} = google_token
+  //   getGoogleDataByUser({id_token, access_token})
+  //   .then((google_data) => {
+  //     /*запись в коллекцию Google res.data
+  //       { id:, //via google_id
+  //         user_id: , // к какому user относится
+  //         email:,
+  //         first_name:,
+  //         last_name:,
+  //         picture:,
+  //         locale:,
+  //         verify:
+  //       }
+  //     */
+  //     const jwt_token = jwt.sign(google_data.data.id, JWT_SECRET)
+  //     return res.send({'token': jwt_token})
+  //   })
+  //   .catch((error) => {
+  //     console.error(`Failed to fetch user`);
+  //     throw new Error(error.message);
+  //   });  
+  // }).catch((error) => {
+  //   console.error(`Failed to getToken`);
+  //   throw new Error(error.message);
+  // });
 }
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const {getCurrentUser, getGoogleUserFromCode, getGoogleAuthURL} = require('../controller/authController');
-const {REDIRECT_URI, SERVER_ROOT_URI,UI_ROOT_URI} = require ('../config');
+const {getFacebookUserFromCode, getFacebookAuthURL} = require('../controller/authController');
+const {GOOGLE_REDIRECT_URI, FACEBOOK_REDIRECT_URI, SERVER_ROOT_URI, UI_ROOT_URI} = require ('../config');
 
 /* temorary */
 var axios = require('axios');
@@ -12,17 +13,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express'});
 });
 
-// Getting login URL
+// Getting login URL BY GOOGLE
 router.get("/auth/google/url", (req, res) => {
   return res.send(getGoogleAuthURL());
 });
 
 // Getting the user from Google with the code
-router.get(`/${REDIRECT_URI}`, (request, response)=>{
+router.get(`/${GOOGLE_REDIRECT_URI}`, (request, response)=>{
   const code = request.query.code.toString();
   axios
   .post(
-    `${SERVER_ROOT_URI}/${REDIRECT_URI}`
+    `${SERVER_ROOT_URI}/${GOOGLE_REDIRECT_URI}`
     , {code:code}
   ).then((res)=>{
     console.log('res',res.data)
@@ -30,7 +31,31 @@ router.get(`/${REDIRECT_URI}`, (request, response)=>{
   })
   
 })
-router.post(`/${REDIRECT_URI}`, getGoogleUserFromCode)
+router.post(`/${GOOGLE_REDIRECT_URI}`, getGoogleUserFromCode)
+
 router.get("/auth/me", getCurrentUser);
+
+// Getting login URL BY FACEBOOK
+router.get("/auth/facebook/url", (req, res) => {
+  return res.send(getFacebookAuthURL());
+});
+
+router.get(`/${FACEBOOK_REDIRECT_URI}`, (request, response)=>{
+  const code = request.query.code.toString();
+  axios
+  .post(
+    `${SERVER_ROOT_URI}/${FACEBOOK_REDIRECT_URI}`
+    , {code:code}
+  ).then((res)=>{
+    console.log('res',res.data)
+    response.redirect(UI_ROOT_URI)
+  })
+})
+
+router.post(`/${FACEBOOK_REDIRECT_URI}`, getFacebookUserFromCode)
+
+
+
+
 
 module.exports = router;
